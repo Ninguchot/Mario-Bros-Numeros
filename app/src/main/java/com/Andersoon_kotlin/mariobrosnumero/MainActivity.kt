125e93882981240ea2d61aeb7b25b74b9e0311b7
And <anderson.almeida.21012@hotmail.com>
2023-10-22 11:24:01 -0300

    Esconder o StatusBar

--- app/src/main/java/com/Andersoon_kotlin/mariobrosnumero/MainActivity.kt
+++ app/src/main/java/com/Andersoon_kotlin/mariobrosnumero/MainActivity.kt
@@ -2,10 +2,18 @@ package com.Andersoon_kotlin.mariobrosnumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.Andersoon_kotlin.mariobrosnumero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN // Esconder o StatusBar
     }
 }
