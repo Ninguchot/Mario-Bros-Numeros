716bc4b233e7ab18840c0febe848d01d99ed9d86
And <anderson.almeida.21012@hotmail.com>
2023-10-26 21:56:20 -0300

    activity_game_over.xml feito

--- app/src/main/java/com/Andersoon_kotlin/mariobrosnumero/GameOver.kt
+++ app/src/main/java/com/Andersoon_kotlin/mariobrosnumero/GameOver.kt
@@ -7,5 +7,8 @@ class GameOver : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
+
+
+        fsdfsdfsdfdsfdsfsfsafasdfdfafsdfasfasdfdsaffdsdfa
     }
 }
